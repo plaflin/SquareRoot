12578968bcc44cb089136fbb95d25ebd3879db43
plaflin <patricklaflin@gmail.com>
2019-07-28 12:09:21 -0400

    Initial Upload
    Value is hard coded in for now. I left a TODO to go back in and get user input instead.

---
+++ Root/src/main/java/com/patricklaflin/Root.java
@@ -0,0 +1,10 @@
+package com.patricklaflin;
+
+public class Root {
+    public static void main(String[] args) {
+        // Returns the square root of the hard coded value
+        // TODO: PBL - Remove hard coded value and replace with user input value
+        int number = 225;
+        System.out.println("The square root of " +number+ " is " +Math.sqrt(number));
+    }
+}
